57f4529ed46e4008fab22c41300297d90d76e3d5
vassalos <vassalos@iridalabs.gr>
2021-04-26 12:13:54 +0300

    updateded script for automated install on arm64

--- install-synergy-for-arm64.sh
+++ install-synergy-for-arm64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo dpkg -i ./libavahi-compat-libdnssd1_0.6.32_rc+dfsg-1ubuntu2_arm64.deb
-sudo dpkg -i ./libcrypto++9v5_5.6.1-9ubuntu0.1_arm64.deb
-sudo dpkg -i ./synergy_1.6.2-0ubuntu2_arm64.deb
+sudo dpkg -i ./Synergy-for-Ubuntu/arm64/libavahi-compat-libdnssd1_0.6.32_rc+dfsg-1ubuntu2_arm64.deb
+sudo dpkg -i ./Synergy-for-Ubuntu/arm64/libcrypto++9v5_5.6.1-9ubuntu0.1_arm64.deb
+sudo dpkg -i ./Synergy-for-Ubuntu/arm64/synergy_1.6.2-0ubuntu2_arm64.deb
